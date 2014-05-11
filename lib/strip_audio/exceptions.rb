63f154dbd940e1d81b44ec9978883e6c7cfc6b82
Casey Robinson <kc@rampantmonkey.com>
2014-05-10 21:41:18 -0400

    one line exceptions

--- lib/strip_audio/exceptions.rb
+++ lib/strip_audio/exceptions.rb
@@ -1,18 +1,13 @@
 module StripAudio
 
-  class FfmpegMissing < StandardError
-  end
+  class FfmpegMissing < StandardError; end
 
-  class FfmpegEncodingError < StandardError
-  end
+  class FfmpegEncodingError < StandardError; end
 
-  class InvalidSourceFile < StandardError
-  end
+  class InvalidSourceFile < StandardError; end
 
-  class AudioFileAlreadyExists < StandardError
-  end
+  class AudioFileAlreadyExists < StandardError; end
 
-  class InValidArguments < StandardError
-  end
+  class InValidArguments < StandardError; end
 
 end
